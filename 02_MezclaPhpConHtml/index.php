ad0f3cfaf2b17a96af54bce6b5e00ebd5aaf60b0
David Márquez <david.marquez.cordoba@gmail.com>
2024-11-13 20:52:46 +0100

    uso de print en php y los saltos de línea

---
+++ 02_MezclaPhpConHtml/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    print "Bienvenidos a mi primera página de PHP <br>";
+    print "He realizado un salto de línea con br <br>";
+    print "Adiós";
+  ?>
+</body>
+</html>
