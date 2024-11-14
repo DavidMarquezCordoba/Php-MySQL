d4710bbb276fb224727cbbff7c4ee8088ae943ab
David Márquez <david.marquez.cordoba@gmail.com>
2024-11-15 00:39:26 +0100

    Operadores matemáticos II

---
+++ 11_OperadoresMatematicos_II/logica.php
@@ -0,0 +1,36 @@
+<?php
+
+  //Recogemos los datos del formulario
+  if(isset($_POST["button"])){
+    $num1=$_POST["num1"];
+    $num2=$_POST["num2"];
+    $operacion=$_POST["operacion"];
+
+    //Pasamos por parámetros las 3 variables necesari
+    calcula($operacion, $num1, $num2);
+  }
+  //Definimos 3 parámetros necesarios para le resultado
+  function calcula($operacion, $num1, $num2){
+
+    switch ($operacion) {
+      case 'suma':
+        $resultado = $num1 + $num2;
+        break;
+      case 'resta':
+        $resultado = $num1 - $num2;
+        break;
+      case 'multiplicacion':
+        $resultado = $num1 * $num2;
+        break;
+      case 'division':
+        $resultado = $num1 / $num2;
+        break;
+      case 'modulo':
+        $resultado = $num1 % $num2;
+        break;
+    }
+
+    echo "El resultado es $resultado";
+  }
+
+?>
